2455a6dbe97377529ece9a2d63b5c944be7dad7b
Alexander Olofsson <ace@haxalot.com>
2020-05-11 20:14:08 +0200

    Improve mirror methods slightly

--- lib/sabayon_linux/mirror.rb
+++ lib/sabayon_linux/mirror.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require 'net/http'
+require 'time'
 
 module SabayonLinux
   class Mirror
-    CHECK_INTERVAL = 8 * 60 * 60
+    CONNECTION_CHECK_INTERVAL = 8 * 60 * 60 # every eight hours
+    TIMESTAMP_CHECK_INTERVAL = 30 * 60 # 2/hour
 
     attr_accessor :name, :country, :speed,
                   :ftp_servers, :http_servers, :rsync_servers,
@@ -22,13 +24,24 @@ module SabayonLinux
       @status = params.fetch(:status, :unknown)
       @failed_checks = params.fetch(:failed_checks, 0)
       @last_check = params.fetch(:last_check, Time.at(0))
+      @last_check = Time.at(@last_check) if @last_check.is_a? Numeric
+      @last_check = Time.parse(@last_check) unless @last_check.is_a? Time
       @next_check = params.fetch(:next_check, Time.now)
+      @next_check = Time.at(@next_check) if @next_check.is_a? Numeric
+      @next_check = Time.parse(@next_check) unless @next_check.is_a? Time
+      @next_timestamp_check = params.fetch(:next_timestamp_check, Time.now)
+      @next_timestamp_check = Time.at(@next_timestamp_check) if @next_timestamp_check.is_a? Numeric
+      @next_timestamp_check = Time.parse(@next_timestamp_check) unless @next_timestamp_check.is_a? Time
     end
 
     def check_connection
       return status if Time.now < next_check
+      return :no_http unless http_servers&.any?
 
       @last_check = Time.now
+      available = []
+
+      # TODO: Test if https is also available
 
       attempts = 0
       available = http_servers.map do |base_url|
@@ -51,7 +64,7 @@ module SabayonLinux
 
           resp.value
           resp.body.to_i
-        rescue Net::HTTPRequestTimeOut, Net::HTTPError
+        rescue Net::HTTPRequestTimeOut, Net::HTTPServerException, Net::HTTPError, Net::OpenTimeout, Errno::ECONNREFUSED, SocketError
           nil
         ensure
           attempts = 0
@@ -68,8 +81,8 @@ module SabayonLinux
       else
         @failed_checks += 1
         if @failed_checks == 1
-          # On the first failure, schedule a recheck in an hour
-          backoff = -CHECK_INTERVAL + 1 * 60 * 60
+          # On the first failure, schedule a recheck in half an hour
+          backoff = -CONNECTION_CHECK_INTERVAL + 30 * 60
         else
           # On repeated failures, schedule the next check later, up to a 24h interval
           backoff = [@failed_checks - 1, 8].min * 2 * 60 * 60
@@ -77,9 +90,7 @@ module SabayonLinux
       end
 
       @status = curr_status ? :online : :unreachable
-      @next_check = Time.now + CHECK_INTERVAL + backoff
-
-      @status
+      @next_check = Time.now + CONNECTION_CHECK_INTERVAL + backoff
     end
 
     def test_speed(size: :small)
@@ -122,7 +133,7 @@ module SabayonLinux
     end
 
     def timestamp
-      return Time.at(@timestamp) if @timestamp
+      return Time.at(@timestamp) if @timestamp && (@next_timestamp_check || Time.at(0)) < Time.now
 
       timestamp!
     end
@@ -141,11 +152,12 @@ module SabayonLinux
 
           resp.value
           resp.body.to_i
-        rescue Net::HTTPRequestTimeOut, Net::HTTPError
+        rescue StandardError
           nil
         end
       end.compact.max
 
+      @next_timestamp_check = Time.now + TIMESTAMP_CHECK_INTERVAL
       Time.at(@timestamp) if @timestamp
     end
 
@@ -153,6 +165,26 @@ module SabayonLinux
       check_connection == :online
     rescue StandardError
       false
+    end
+
+    def ftp_servers?
+      ftp_servers&.any?
+    end
+
+    def http_servers?
+      http_servers&.any?
+    end
+
+    def https_servers
+      http_servers&.select { |u| u.start_with? 'https://' }
+    end
+
+    def https_servers?
+      https_servers&.any?
+    end
+
+    def rsync_servers?
+      rsync_servers&.any?
     end
 
     def to_json(*params)
@@ -167,8 +199,9 @@ module SabayonLinux
 
         status: status,
         failed_checks: @failed_checks,
-        last_check: last_check,
-        next_check: next_check
+        last_check: last_check.to_i,
+        next_check: next_check.to_i,
+        next_timestamp_check: @next_timestamp_check.to_i
       }.compact.to_json(*params)
     end
   end
